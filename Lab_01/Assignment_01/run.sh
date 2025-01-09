12f2460e40163d3e7881e29205d4ff14a49ee84b
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2025-01-09 02:03:01 +0200

    added blueprint for bash script that builds and launches the assignment app

---
+++ Lab_01/Assignment_01/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# run scripts to generate log files
+
+# build using cmake
+
+# copy elf file to ASSIGNMENT_01 root dir in order to be able to view log files' locations
+
+# launch elf file
